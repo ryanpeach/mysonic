4a86f6d6c14632e3bece1752f28f599e1cf4e777
Ryan Peach <ryan.peach@keysight.com>
2018-07-29 11:07:53 -0400

    Added deletions to download.sh

--- data/sprites/download.sh
+++ data/sprites/download.sh
@@ -2,3 +2,8 @@ curl http://www.sonicgalaxy.net/wp-content/img/sprites/gen/sonic/ | \
 ack -o '(?<=\").*.png(?=\")' | \
 awk '$0="http://www.sonicgalaxy.net/wp-content/img/sprites/gen/sonic/"$0' | \
 xargs wget
+rm *zone*.png
+rm *stage*.png
+rm *font*.png
+rm *logo*.png
+rm *title*.png
