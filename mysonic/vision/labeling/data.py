e2184df3821e4fcdff206937e3236c53513eddac
Ryan Peach <ryan.peach@outlook.com>
2018-07-31 20:33:06 -0400

    working with random padding

--- mysonic/vision/labeling/data.py
+++ mysonic/vision/labeling/data.py
@@ -1,20 +1,30 @@
 import torch
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.optim import lr_scheduler
 import torchvision
-from torchvision import transforms, datasets
+from torchvision import transforms, models, datasets
+from torchvision.transforms import functional as F
 from random import randrange
 from pathlib import Path
 import numpy as np
 from PIL import Image
 import matplotlib.pyplot as plt
-import pdb
+import ipdb
+import os
+import time
+import copy
 
-IMAGE_SIZE = (50, 50)
+IMAGE_SIZE = (256, 256)
+
 
 def pil_loader(path):
     # open path as file to avoid ResourceWarning (https://github.com/python-pillow/Pillow/issues/835)
     with open(path, 'rb') as f:
         img = Image.open(f)
         return img.convert('RGBA')
+
 
 class ApplyToBackground():
     def __init__(self, path):
@@ -24,11 +34,11 @@ class ApplyToBackground():
         w0, h0 = img.size  # PIL does this backwards
 
         # Pick a random background and crop it
-        i = randrange(0,len(self.backgrounds))
+        i = randrange(0, len(self.backgrounds))
         bg = self.backgrounds[i].copy()
         bg = transforms.RandomCrop((h0, w0))(bg)
-
-        return Image.alpha_composite(bg, img).convert("RGB")
+        bg.paste(img, mask=img)
+        return bg.convert("RGB")
 
 
 sprites_transform = transforms.Compose([
@@ -39,13 +49,17 @@ sprites_transform = transforms.Compose([
         transforms.Normalize(mean=[0.485, 0.456, 0.406],
                              std=[0.229, 0.224, 0.225])
     ])
-sprites_dataset = datasets.ImageFolder(root='data/sprites/classes/',
-                                       transform=sprites_transform,
-                                       loader=pil_loader)
-sprites_loader = torch.utils.data.DataLoader(sprites_dataset,
-                                             batch_size=8, shuffle=True,
-                                             num_workers=4)
-class_names = sprites_dataset.classes
+data_dir = 'data/sprites/classes/'
+image_datasets = {x: datasets.ImageFolder(data_dir,
+                                          sprites_transform,
+                                          loader=pil_loader)
+                  for x in ['train', 'val']}
+dataloaders = {x: torch.utils.data.DataLoader(image_datasets[x], batch_size=4,
+                                              shuffle=True, num_workers=4)
+               for x in ['train', 'val']}
+dataset_sizes = {x: len(image_datasets) for x in ['train', 'val']}
+class_names = image_datasets['train'].classes
+
 
 def imshow(inp, title=None):
     """Imshow for Tensor."""
@@ -59,9 +73,149 @@ def imshow(inp, title=None):
         plt.title(title)
     plt.pause(5)  # pause a bit so that plots are updated
 
-#device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-#print(device)
-
-inputs, classes = next(iter(sprites_loader))
+inputs, classes = next(iter(dataloaders['train']))
 out = torchvision.utils.make_grid(inputs)
 imshow(out, title=[class_names[x] for x in classes])
+
+device = torch.device("cpu")
+#print("Device:", device)
+
+def train_model(model, criterion, optimizer, scheduler, num_epochs=25):
+    since = time.time()
+
+    best_model_wts = copy.deepcopy(model.state_dict())
+    best_acc = 0.0
+
+    for epoch in range(num_epochs):
+        print('Epoch {}/{}'.format(epoch, num_epochs - 1))
+        print('-' * 10)
+
+        # Each epoch has a training and validation phase
+        for phase in ['train', 'val']:
+            if phase == 'train':
+                scheduler.step()
+                model.train()  # Set model to training mode
+            else:
+                model.eval()   # Set model to evaluate mode
+
+            running_loss = 0.0
+            running_corrects = 0
+
+            # Iterate over data.
+            for inputs, labels in dataloaders[phase]:
+                inputs = inputs.to(device)
+                labels = labels.to(device)
+
+                # zero the parameter gradients
+                optimizer.zero_grad()
+
+                # forward
+                # track history if only in train
+                with torch.set_grad_enabled(phase == 'train'):
+                    outputs = model(inputs)
+                    _, preds = torch.max(outputs, 1)
+                    loss = criterion(outputs, labels)
+
+                    # backward + optimize only if in training phase
+                    if phase == 'train':
+                        loss.backward()
+                        optimizer.step()
+
+                # statistics
+                running_loss += loss.item() * inputs.size(0)
+                running_corrects += torch.sum(preds == labels.data)
+
+            epoch_loss = running_loss / dataset_sizes[phase]
+            epoch_acc = running_corrects.double() / dataset_sizes[phase]
+
+            print('{} Loss: {:.4f} Acc: {:.4f}'.format(
+                phase, epoch_loss, epoch_acc))
+
+            # deep copy the model
+            if phase == 'val' and epoch_acc > best_acc:
+                best_acc = epoch_acc
+                best_model_wts = copy.deepcopy(model.state_dict())
+
+        print()
+
+    time_elapsed = time.time() - since
+    print('Training complete in {:.0f}m {:.0f}s'.format(
+        time_elapsed // 60, time_elapsed % 60))
+    print('Best val Acc: {:4f}'.format(best_acc))
+
+    # load best model weights
+    model.load_state_dict(best_model_wts)
+    return model
+
+
+def visualize_model(model, num_images=6):
+    was_training = model.training
+    model.eval()
+    images_so_far = 0
+    fig = plt.figure()
+
+    with torch.no_grad():
+        for i, (inputs, labels) in enumerate(dataloaders['val']):
+            inputs = inputs.to(device)
+            labels = labels.to(device)
+
+            outputs = model(inputs)
+            _, preds = torch.max(outputs, 1)
+
+            for j in range(inputs.size()[0]):
+                images_so_far += 1
+                ax = plt.subplot(num_images//2, 2, images_so_far)
+                ax.axis('off')
+                ax.set_title('predicted: {}'.format(class_names[preds[j]]))
+                imshow(inputs.cpu().data[j])
+
+                if images_so_far == num_images:
+                    model.train(mode=was_training)
+                    return
+        model.train(mode=was_training)
+
+
+model_ft = models.resnet18(pretrained=True)
+num_ftrs = model_ft.fc.in_features
+model_ft.fc = nn.Linear(num_ftrs, 2)
+
+model_ft = model_ft.to(device)
+
+criterion = nn.CrossEntropyLoss()
+
+# Observe that all parameters are being optimized
+optimizer_ft = optim.SGD(model_ft.parameters(), lr=0.001, momentum=0.9)
+
+# Decay LR by a factor of 0.1 every 7 epochs
+exp_lr_scheduler = lr_scheduler.StepLR(optimizer_ft, step_size=7, gamma=0.1)
+
+model_ft = train_model(model_ft, criterion, optimizer_ft, exp_lr_scheduler,
+                       num_epochs=25)
+visualize_model(model_ft)
+
+model_conv = torchvision.models.resnet18(pretrained=True)
+for param in model_conv.parameters():
+    param.requires_grad = False
+
+# Parameters of newly constructed modules have requires_grad=True by default
+num_ftrs = model_conv.fc.in_features
+model_conv.fc = nn.Linear(num_ftrs, 2)
+
+model_conv = model_conv.to(device)
+
+criterion = nn.CrossEntropyLoss()
+
+# Observe that only parameters of final layer are being optimized as
+# opoosed to before.
+optimizer_conv = optim.SGD(model_conv.fc.parameters(), lr=0.001, momentum=0.9)
+
+# Decay LR by a factor of 0.1 every 7 epochs
+exp_lr_scheduler = lr_scheduler.StepLR(optimizer_conv, step_size=7, gamma=0.1)
+
+model_conv = train_model(model_conv, criterion, optimizer_conv,
+                         exp_lr_scheduler, num_epochs=25)
+
+visualize_model(model_conv)
+
+plt.ioff()
+plt.show()
